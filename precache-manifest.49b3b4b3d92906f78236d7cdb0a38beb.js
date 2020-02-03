78ba7f38fcb1f52e5e2c0fdb30a641f15e5ee9aa
markus emile <markusemile@gmail.com>
2020-02-03 18:28:05 +0100

    Updates

---
+++ precache-manifest.49b3b4b3d92906f78236d7cdb0a38beb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f2eb3b04831d5fa4c1023d9dca5508",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "8881efcf13105acc64de",
+    "url": "/react/static/css/main.bcb42518.chunk.css"
+  },
+  {
+    "revision": "68a99abd71647c51021e",
+    "url": "/react/static/js/2.ffb4dcf0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react/static/js/2.ffb4dcf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8881efcf13105acc64de",
+    "url": "/react/static/js/main.7753987c.chunk.js"
+  },
+  {
+    "revision": "cb6ee3d0b480856ca108",
+    "url": "/react/static/js/runtime-main.3e4a0562.js"
+  },
+  {
+    "revision": "dc143a47093087cb337a3b2b9a9f3aa5",
+    "url": "/react/static/media/image 2.1.dc143a47.png"
+  },
+  {
+    "revision": "377d97067cfd2cd6d0b903a52a271285",
+    "url": "/react/static/media/image.377d9706.png"
+  },
+  {
+    "revision": "7d5b60c63c3efb9ec4fea4378c37a9e6",
+    "url": "/react/static/media/wood-left.7d5b60c6.png"
+  },
+  {
+    "revision": "b889fe50555acf39620a2ca8101bbf6e",
+    "url": "/react/static/media/wood-restaurant.b889fe50.png"
+  },
+  {
+    "revision": "ae50cb0dab66846e0a45547309cb9a3e",
+    "url": "/react/static/media/wood-right.ae50cb0d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/woodstock/precache-manifest.c8362081c41251aac9b781b6988d8eba.js"
+  "/react/precache-manifest.49b3b4b3d92906f78236d7cdb0a38beb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/woodstock/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
